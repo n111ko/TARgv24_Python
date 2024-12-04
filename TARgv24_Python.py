0683cbdd4be174882e77515f08d63f70bc73f13c
niko <niko.aoapp@gmail.com>
2024-12-04 18:05:17 +0200

    Tund 1

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -4,33 +4,46 @@ from math import *
 from tkinter import * #pi kasutamiseks
 
 
-#Ülesanne 1
+##### Ülesanne 1 #####
 print("Tere tulemast!")
+
 nimi=input("Mis on sinu nimi? ").capitalize() #lower()-aaa,upper()-AAA,capitalize()-Aaa
-print("Tere tulemast! Tervitan sind ", nimi) 
+
+print("Tere tulemast! Tervitan sind", nimi) 
 print("Tere tulemast! Tervitan sind "+ nimi)
+
 vanus=int(input("Kui vana sa oled? "))
-print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
-print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+
+print("Tere tulemast! Tervitan sind "+nimi+". Sa oled",vanus,"aastat vana.")
+print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana.")
+######################
 
 
-# #Ülesanne 2
+
+# ##### Ülesanne 2 #####
 # vanus = 18
 # eesnimi = "Jaak" 
 # pikkus = 16.5
 # kas_käib_koolis = True
 # print(type(vanus))
+# ######################
 
-# #Ülesanne 3
+
+
+# ##### Ülesanne 3 #####
 # kokku=randint(1,1000)
 # print(f"Kokku on {kokku} kommi")
 # kommi=int(input("Mitu kommi sa tahad? "))
 # kokku=kokku-kommi
 # print(f"Jääk on {kokku} kommi")
+# ######################
 
-# #Ülesanne 4
+
+
+# ##### Ülesanne 4 #####
 # print("Läbimõõdu leidmine ")
 # #l-ümbermõõt
 # l=float(input("Ümbermõõt: "))
 # d=l/pi
 # print(f"Läbimõõdu suurus on {round(d,2)}")
+# ######################
