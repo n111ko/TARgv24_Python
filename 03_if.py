e910de587547fd61ba44f0f4f6ecfbbb50c20994
niko <niko.aoapp@gmail.com>
2025-01-06 00:05:45 +0200

    Tund 3

---
+++ 03_if.py
@@ -0,0 +1,35 @@
+##### Ülesanne 1 #####
+
+nimi = input("Mis on sinu nimi? ")
+
+if nimi.isalpha() and nimi.isupper():
+    if nimi == "JUKU":
+        print("Lähme kinno")
+        try:
+            vanus = int(input(f"{nimi}, kui vana sa oled? "))
+            if vanus < 0 or vanus > 100:
+                print("Viga!")
+            elif 6 <= vanus <= 14:
+                print("Lastepilet")
+            elif 15 <= vanus <= 65:
+                print ("Täispilet")
+            elif vanus > 65:
+                print("Sooduspilet")
+            else:
+                print("Nii palju!!")
+        except:
+            print("Täisarv on vaja sisestada.")
+else:
+    print("Segatud sõne")
+
+######################
+
+
+
+##### Ülesanne 2 #####
+
+
+
+
+
+######################
